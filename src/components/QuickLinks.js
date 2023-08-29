84fb1a52df6a9fe4ffa32b90a139b1ce2aabad2e
rakib-netmow <rakib.netmow@gmail.com>
2023-08-29 10:00:52 +0600

    first commit

---
+++ src/components/QuickLinks.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./QuickLinks.css";
+
+const QuickLinks = () => {
+  return (
+    <div className="container">
+      <h2>Quick Links</h2>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 1</li>
+        <li>Link 1</li>
+        <li>Link 1</li>
+        <li>Link 1</li>
+      </ul>
+    </div>
+  );
+};
+
+export default QuickLinks;
